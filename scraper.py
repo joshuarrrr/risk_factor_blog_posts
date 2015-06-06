b0dbdfed3a5bdf200ef1df63ae31b1272573315b
joshuarrrr <josh@joshromero.com>
2015-06-05 17:06:01 -0700

    initial migration

--- scraper.py
+++ scraper.py
@@ -1,7 +1,46 @@
+import scraperwiki
+import sys, requests, bs4
+
+# import os, errno, optparse
+
+root = 'http://spectrum.ieee.org'
+baseUrl = root + '/blog/riskfactor/page/'              # starting url
+linkSelector = 'article > a'
+
+sitemap = root + '/sitemap.xml'
+sitemapFilter = 'riskfactor'
+
+links = []
+
+def parseSitemap():
+    res = requests.get(sitemap)
+    res.raise_for_status()
+
+    soup = bs4.BeautifulSoup(res.text)
+    potentialLinks = soup.select('loc')
+
+    for link in potentialLinks:
+        if sitemapFilter in link.string:
+            links.append(link.string)
+            print link.string
+
+    saveLinks()
+    
+def saveLinks():
+    # Save the links to SQLite database
+    uniques = set(links)
+    for item in uniques:
+        scraperwiki.sqlite.save(unique_keys=['url'], data={"url": item})
+    linksFile.close()
+
+    print 'Done.'
+    
+parseSitemap()
+
 # This is a template for a Python scraper on morph.io (https://morph.io)
 # including some code snippets below that you should find helpful
 
-# import scraperwiki
+
 # import lxml.html
 #
 # # Read in a page
